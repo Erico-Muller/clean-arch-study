f598f1ad591b61987efbe2d02857de80b7bda9c2
Erico-Muller <ericomuller0@gmail.com>
2022-11-02 20:54:12 -0300

    tests added to cat controller and service

--- src/modules/cats/infra/http/nestjs/cats.controller.spec.ts
+++ src/modules/cats/infra/http/nestjs/cats.controller.spec.ts
@@ -1,6 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing'
+
 import { CatController } from './cats.controller'
 import { CatService } from './cats.service'
+
+import { CatRepository } from '../../../repository/cat.repository.interface'
+import { CatInMemoryRepository } from '../../db/in-memory/cat-in-memory.repository'
+
+import {
+   CreateCatUseCase,
+   FindAllCatsUseCase,
+   FindOneCatUseCase,
+   HaveABirthdayCatUseCase,
+   DeleteCatUseCase,
+} from '../../../use-cases'
 
 describe('CatsController', () => {
    let controller: CatController
@@ -8,7 +20,48 @@ describe('CatsController', () => {
    beforeEach(async () => {
       const module: TestingModule = await Test.createTestingModule({
          controllers: [CatController],
-         providers: [CatService],
+         providers: [
+            CatService,
+            {
+               provide: CatInMemoryRepository,
+               useClass: CatInMemoryRepository,
+            },
+            {
+               provide: CreateCatUseCase,
+               useFactory: (catRepository: CatRepository) => {
+                  return new CreateCatUseCase(catRepository)
+               },
+               inject: [CatInMemoryRepository],
+            },
+            {
+               provide: FindAllCatsUseCase,
+               useFactory: (catRepository: CatRepository) => {
+                  return new FindAllCatsUseCase(catRepository)
+               },
+               inject: [CatInMemoryRepository],
+            },
+            {
+               provide: FindOneCatUseCase,
+               useFactory: (catRepository: CatRepository) => {
+                  return new FindOneCatUseCase(catRepository)
+               },
+               inject: [CatInMemoryRepository],
+            },
+            {
+               provide: HaveABirthdayCatUseCase,
+               useFactory: (catRepository: CatRepository) => {
+                  return new HaveABirthdayCatUseCase(catRepository)
+               },
+               inject: [CatInMemoryRepository],
+            },
+            {
+               provide: DeleteCatUseCase,
+               useFactory: (catRepository: CatRepository) => {
+                  return new DeleteCatUseCase(catRepository)
+               },
+               inject: [CatInMemoryRepository],
+            },
+         ],
       }).compile()
 
       controller = module.get<CatController>(CatController)
--- src/modules/cats/infra/http/nestjs/cats.service.spec.ts
+++ src/modules/cats/infra/http/nestjs/cats.service.spec.ts
@@ -1,12 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing'
+
 import { CatService } from './cats.service'
+
+import { CatRepository } from '../../../repository/cat.repository.interface'
+import { CatInMemoryRepository } from '../../db/in-memory/cat-in-memory.repository'
+
+import {
+   CreateCatUseCase,
+   FindAllCatsUseCase,
+   FindOneCatUseCase,
+   HaveABirthdayCatUseCase,
+   DeleteCatUseCase,
+} from '../../../use-cases'
 
 describe('CatsService', () => {
    let service: CatService
 
    beforeEach(async () => {
       const module: TestingModule = await Test.createTestingModule({
-         providers: [CatService],
+         providers: [
+            CatService,
+            {
+               provide: CatInMemoryRepository,
+               useClass: CatInMemoryRepository,
+            },
+            {
+               provide: CreateCatUseCase,
+               useFactory: (catRepository: CatRepository) => {
+                  return new CreateCatUseCase(catRepository)
+               },
+               inject: [CatInMemoryRepository],
+            },
+            {
+               provide: FindAllCatsUseCase,
+               useFactory: (catRepository: CatRepository) => {
+                  return new FindAllCatsUseCase(catRepository)
+               },
+               inject: [CatInMemoryRepository],
+            },
+            {
+               provide: FindOneCatUseCase,
+               useFactory: (catRepository: CatRepository) => {
+                  return new FindOneCatUseCase(catRepository)
+               },
+               inject: [CatInMemoryRepository],
+            },
+            {
+               provide: HaveABirthdayCatUseCase,
+               useFactory: (catRepository: CatRepository) => {
+                  return new HaveABirthdayCatUseCase(catRepository)
+               },
+               inject: [CatInMemoryRepository],
+            },
+            {
+               provide: DeleteCatUseCase,
+               useFactory: (catRepository: CatRepository) => {
+                  return new DeleteCatUseCase(catRepository)
+               },
+               inject: [CatInMemoryRepository],
+            },
+         ],
       }).compile()
 
       service = module.get<CatService>(CatService)
